f993c83ab8ca154402cc654b54d4473d229bdd4e
Chenye Ding <dingchenye@juran.com.cn>
2017-11-10 23:55:37 +0800

    crtip bak

---
+++ src/main/java/com/bdsoft/datamin/fetch/airt/jd/JDAirtFetcher.java
@@ -0,0 +1,15 @@
+package com.bdsoft.datamin.fetch.airt.jd;
+
+/**
+ * 功能
+ *
+ * @version 1.0
+ * @auth 丁辰叶
+ * @date 2017/11/10 12:26
+ */
+public class JDAirtFetcher {
+
+    public static void main(String[] args) {
+
+    }
+}
